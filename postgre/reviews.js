49d089720e392da1625bf062e8e84b7d0334f86f
Henri Hyvärinen <Henri.hyvarinen1@gmail.com>
2023-11-21 12:03:52 +0200

    Postgre queryja

---
+++ postgre/reviews.js
@@ -0,0 +1,42 @@
+const pgPool = require("./connection");
+
+const sql = {
+    GET_REVIEWS: "SELECT * FROM review",
+    GET_REVIEW: "SELECT * FROM review WHERE account_id = $1",
+    POST_REVIEW: "INSERT INTO review (account_id, text, movie_id, rating) VALUES  ($1, $2, $3, $4)"
+    
+  };
+    //Hakee kaikki arvostelut
+  //getReviews();
+  async function getReviews() {
+    try {
+        const result = await pgPool.query(sql.GET_REVIEWS);
+        const rows = result.rows;
+        return console.log(rows);
+        
+    } catch (error) {
+        console.error("Error executing query:", error);
+    }
+}
+//Hakee arvostelut account_id avulla (TESTI)
+//getReview(10);
+
+async function getReview(account_id) {
+    try {
+        const result = await pgPool.query(sql.GET_REVIEW,[account_id]);
+        const rows = result.rows;
+        return console.log(rows);
+        
+    } catch (error) {
+        console.error("Error executing query:", error);
+    }
+}
+//asettaa arvot databaseen (TESTI)
+//PostReview (9, "huono leffa", 23, 1);    
+
+async function PostReview(account_id, text, movie_id, rating) {
+    await pgPool.query(sql.POST_REVIEW, [account_id,text,movie_id,rating ]);
+
+}
+
+module.exports={getReviews, getReview, PostReview};
