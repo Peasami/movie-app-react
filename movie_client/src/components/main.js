0cad792c5706295a70b83507bcf016e7e2f31945
lukas7251 <lukas.kalliokoski@gmail.com>
2023-11-22 15:45:07 +0200

    home page trending fetch data

--- movie_client/src/components/main.js
+++ movie_client/src/components/main.js
@@ -1,4 +1,5 @@
-
+import { Link, Outlet } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 
 function Main() {
     return (
@@ -10,18 +11,67 @@ function Main() {
     );
 }
 
-function Trending() {
+function TrendingTitle({ movie }) {
     return (
-        <div>
-            <h1>Trending Movies</h1>
-        </div>
+        <li><b>{movie.title}</b>
+            <br />
+            {movie.overview}
+        </li>
+    );
+}
+
+function Trending() {
+    const [trendingMovies, setTrendingMovies] = useState([]);
+
+
+    const API_KEY = process.env.REACT_APP_TMBD_API_KEY;
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: "Bearer " + API_KEY,
+                },
+            };
+            try {
+                const response = await fetch(
+                    'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+                    options
+                );
+                const data = await response.json();
+                setTrendingMovies(data.results);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+    return (
+        <>
+            <div>
+                <h1>Trending Movies</h1>
+                <div style={{ width: '300px', height: '250px', border: 'solid' }}>
+                    <ul>
+                        {trendingMovies.map((movie) => (
+                            <TrendingTitle movie={movie} />
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
 
 function Reviews() {
     return (
         <div>
-            <h1>Reviews</h1>
+            <h1>
+                <Link to="/reviews" style={{ textDecoration: 'none' }}>Reviews</Link>
+            </h1>
+            <div style={{ width: '300px', height: '250px', border: 'solid', display: 'flex' }}></div>
         </div>
     );
 }
@@ -29,7 +79,10 @@ function Reviews() {
 function Groups() {
     return (
         <div>
-            <h1>Groups</h1>
+            <h1>
+                <Link to="/groups" style={{ textDecoration: 'none' }}>Groups</Link>
+            </h1>
+            <div style={{ width: '300px', height: '250px', border: 'solid' }}></div>
         </div>
     );
 }
