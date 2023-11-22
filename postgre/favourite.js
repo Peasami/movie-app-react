a8efd260ef818cd4132b7f1c8abb35eb4fef1a7a
Henri Hyvärinen <Henri.hyvarinen1@gmail.com>
2023-11-22 15:02:13 +0200

    Start.js ja reitteja

---
+++ postgre/favourite.js
@@ -0,0 +1,31 @@
+const pgPool = require("./connection");
+
+const sql ={
+    GET_FAVOURITE: "SELECT * FROM favourite WHERE account_id = $1",
+    ADD_FAVOURITE: "INSERT INTO favourite (movie_id, account_id) VALUES ($1,$2)"
+};
+
+//hakee account_id perusteella käyttäjän lempileffat
+//getFavourites(9)
+async function getFavourites(account_id){
+    try {
+        const result = await pgPool.query(sql.GET_FAVOURITE, [account_id]);
+        const rows = result.rows;
+        return console.log(rows);
+        
+    } catch (error) {
+        console.error("Error executing query:", error);
+    }
+}
+
+
+//addFavourite("137",9);
+async function addFavourite(movie_id, account_id){
+
+ await pgPool.query(sql.ADD_FAVOURITE, [movie_id, account_id ]);
+
+}
+
+
+
+module.exports = {getFavourites, addFavourite};
--- routes/account.js
+++ routes/account.js
@@ -1,1 +1,45 @@
 // register, login and profile view
+const router = require('express').Router();
+const multer = require('multer');
+const upload = multer({dest:'upload/'});
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { createToken } = require('../Auth/auth');
+
+
+const {register,checkLogin} = require('../postgre/account');
+
+
+router.post("/register", upload.none(), async (req,res) =>{
+    const username = req.body.username;
+    let pw = req.body.pw;
+    
+    pw = await bcrypt.hash(pw,10);
+    try {
+        await register(username,pw);
+        res.end();
+    } catch(error){
+        console.log("Virhe operaatiossa")
+        
+    }
+});
+
+router.post("/login"),upload.none(), async (req,res) => {
+    console.log("Received post request on /register")
+    const username = req.body.username;
+    let pw = req.body.pw
+    
+    const pwHash = checkLogin(username);
+    if (pwHast){
+        const isCorrect = await bcrypt.compare(pw,pwHash);
+        if(isCorrect) {
+            const token = createToken(username);
+            res.status(200).json({jwtToken:token});
+        } else {
+        res.status(401).json({error:"error"});
+        }
+    } else{
+        res.status(401).json({error: "wrong username"})};
+};
+
+module.exports = router;
