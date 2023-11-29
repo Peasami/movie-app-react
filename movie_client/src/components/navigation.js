7c99f7e79b86f6dbd84441aa210f3cd6d7cf0c7f
Santeri <santeri.hyry@gmail.com>
2023-11-29 15:41:43 +0200

    fix main.js

--- movie_client/src/components/navigation.js
+++ movie_client/src/components/navigation.js
@@ -1,20 +1,17 @@
-// Navigation bar at the top
 import { Link, Outlet } from "react-router-dom";
+import  Search from './search.js';
 
 const Navigation = () => {
-    return (
-        <>
-            <nav>
-                <form>
-                    <Link to="/" style={{ color: "red", fontSize: 40, margin: 40 }}>Movie App</Link>
-                    <input type="text" style={{margin: 40 }}></input>
-                    <Link to="/register" style={{marginLeft: 200}}>Register</Link>
-                </form>
-            </nav>
-
-            <Outlet />
-        </>
-    )
-}
+  return (
+    <>
+      <nav>
+        <a href="/">Movie App</a>
+        <Search />
+        <Link to="/register" className="signup-link">SignUp</Link>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
 
 export default Navigation;
