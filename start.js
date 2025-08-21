39e5d67ac7bc36145d781db7cc8e0405507f18b6
Santeri <122432859+Peasami@users.noreply.github.com>
2025-08-21 15:00:06 +0300

    Load dotenv only in non-production environments

--- start.js
+++ start.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express');
 const accountRoute = require('./routes/account');
 const groupsRoute = require('./routes/groups');
