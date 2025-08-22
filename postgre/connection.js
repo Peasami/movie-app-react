466866d490bdc2b53e5ee1a344f2200328030846
Santeri <122432859+Peasami@users.noreply.github.com>
2025-08-22 10:45:58 +0300

    ssl true

--- postgre/connection.js
+++ postgre/connection.js
@@ -10,7 +10,7 @@ const pgPool = new Pool({
   database: process.env.PG_DB,
   user: process.env.PG_USER,
   password: process.env.PG_PW,
-  ssl: process.env.PG_SSL
+  ssl: true
 
 });
 pgPool.connect((err) => {
